6c8576a7a56418e5600a0dbd1e2ad0d941f5c967
Olohi John <ojohn@smith.edu>
2024-02-17 21:49:44 -0500

    Defined basic methods for engine class

--- Engine.java
+++ Engine.java
@@ -1,3 +1,27 @@
+import FuelType;
+//import FuelType;
+
 public class Engine {
+
+    public String fuelType ;
+    public double currentFuel;
+    public double maxFuel;
+
+    public Engine(String fuelType, double currentFuel, double maxFuel){
+        this.fuelType = fuelType;
+        this.currentFuel = currentFuel;
+        this.maxFuel = maxFuel;
+    }
+
+    public void refuel(){
+        this.currentFuel =  this.maxFuel;
+    }
+
+    public boolean go(){
+        double value = 5.00;
+        this.currentFuel -= value;
+        System.out.println(this.currentFuel);
+        return (this.currentFuel > 0);
+    } 
 
 }
